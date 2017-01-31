8c66b8965de50e03c0bab6538a24f01f2caf82ec
najar1412 <rory.jarrel@gmail.com>
2017-01-31 17:51:34 -0500

    functionality

--- patron-app/app.py
+++ patron-app/app.py
@@ -34,5 +34,39 @@ def getuserbyid():
     return render_template('getuserbyid.html', bla=r.json())
 
 
+@app.route('/getpatronbyid', methods=['GET'])
+def getpatronbyid():
+
+    bla = request.args['id']
+
+    r = requests.get('{}/patron/{}'.format(BASEURL, str(bla)))
+
+    return render_template('getpatronbyid.html', bla=r.json())
+
+
+@app.route('/deluser', methods=['GET'])
+def deluser():
+
+    bla = request.args['id']
+
+    del_user = requests.delete('{}/user/delete/{}'.format(BASEURL, int(bla)))
+    print(del_user)
+    r = requests.get('{}/user'.format(BASEURL))
+
+    return render_template('getuser.html', user=r.json())
+
+
+@app.route('/delpatron', methods=['GET'])
+def delpatron():
+
+    bla = request.args['id']
+
+    del_user = requests.delete('{}/patron/delete/{}'.format(BASEURL, int(bla)))
+    r = requests.get('{}/patron'.format(BASEURL))
+
+    return render_template('getpatron.html', patron=r.json())
+
+
+
 if __name__ == '__main__':
     app.run(port=5050, debug=True)
